a0cf9754e5690578563688f85b8cfb02ad102588
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-22 18:20:55 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1 = 5;
+            string var2 = "Hi";
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(var2 +var1);
+        }
+    }
+}
+
